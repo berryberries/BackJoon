3a2e474d0633f1d4f941064cce6a645745b433f1
berryberries <122462263+berryberries@users.noreply.github.com>
2023-07-03 22:58:08 +0900

    [Bronze III] Title: 알람 시계, Time: 232 ms, Memory: 18464 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int H = sc.nextInt();
+		int m = sc.nextInt();
+		
+		if(m<45) {
+			
+			H--;
+			m = 60 - (45-m);
+			
+			if(H<0) {
+				H=23;
+			}
+			System.out.println(H + " " + m);
+		}else {
+			System.out.println(H + " " + (m - 45));
+		}
+
+
+	}
+}
