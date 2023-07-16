148b0cc149a800959c27c26ffbe7d7bf0c3b5932
berryberries <122462263+berryberries@users.noreply.github.com>
2023-07-16 14:05:46 +0900

    [Bronze III] Title: 오븐 시계, Time: 236 ms, Memory: 18468 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main{
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		
+		int time = (60*A) + B + C;
+	
+		A = time/60;
+		B = time%60;
+		
+        if(B>60){
+            A+=1;
+            B-=60;
+        }
+		if(A>=24) {
+			A -=24;
+		}
+		System.out.println(A + " " + B);
+	}
+
+}
